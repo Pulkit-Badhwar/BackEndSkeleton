a79a63d205ed09f4f552fd73c3cee1bc77dfad45
Pulkit-Badhwar <pulkitbadhwar.bsc18@sushantuniversity.edu.in>
2022-01-18 13:24:50 +0530

    validation done

--- web/dashboardRouter/impactUser/createUserHandler.js
+++ web/dashboardRouter/impactUser/createUserHandler.js
@@ -1,38 +1,35 @@
 /* eslint-disable no-useless-catch */
 const { createUser } = require('../../../service/impactUserService');
+const { isCharactersString, isEmailString, isPasswordString } = require('../../../service/validate/regexValidate');
 const crypto = require("crypto");
 
 
 
 async function handler(req) {
 
-  const algorithm = "aes-256-cbc";
-  const initVector = crypto.randomBytes(16);
-  const message = req.body.password;
-  const Securitykey = crypto.randomBytes(32);
+  if (isCharactersString(req.body.name) && isEmailString(req.body.email) && isPasswordString(req.body.password)) {
+    const algorithm = "aes-256-cbc";
+    const initVector = crypto.randomBytes(16);
+    const message = req.body.password;
+    const Securitykey = crypto.randomBytes(32);
+    const cipher = crypto.createCipheriv(algorithm, Securitykey, initVector);
 
-  const cipher = crypto.createCipheriv(algorithm, Securitykey, initVector);
+    let encryptedData = cipher.update(message, "utf-8", "hex");
+    encryptedData += cipher.final("hex");
 
-  let encryptedData = cipher.update(message, "utf-8", "hex");
-  encryptedData += cipher.final("hex");
-
-  // console.log("Encrypted message: " + encryptedData);
-
-
-  const decipher = crypto.createDecipheriv(algorithm, Securitykey, initVector);
-  let decryptedData = decipher.update(encryptedData, "hex", "utf-8");
-  decryptedData += decipher.final("utf8");
-  // console.log("Decrypted message: " + decryptedData);
+    const user = {
+      name: req.body.name,
+      email: req.body.email,
+      password: encryptedData,
+    };
 
 
-  const user = {
-    name: req.body.name,
-    email: req.body.email,
-    password: encryptedData,
-  };
-
-  const result = await createUser(user);
-  return result;
+    const result = await createUser(user);
+    return result;
+  }
+  else {
+    console.log('Data Invalid')
+  }
 }
 
 function createUserHandler(req, res, next) {
