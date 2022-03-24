f8cb5a0c423d042c687b9b2ccde32daf7e79eb0b
Pulkit <pulkitbadhwar2975@gmail.com>
2022-03-24 16:58:10 +0530

    fetchFile

--- web/dashboardRouter/resources/index.js
+++ web/dashboardRouter/resources/index.js
@@ -3,6 +3,7 @@ const fetchAllResourcesHandler = require('./fetchAllResourcesHandler');
 const fetchLibraryByEmailHandler = require('./fetchLibraryByEmailHandler');
 const fetchResourcesImage = require('./fetchResourcesImage');
 const fetchAllImages = require('./fetchAllImages');
+const fetchFileByTopic = require('./fetchFileByTopic');
 
 
 module.exports = (router) => {
@@ -11,4 +12,5 @@ module.exports = (router) => {
   router.get('/resources/fetchAll', fetchAllResourcesHandler);
   router.get('/resources/fetchImage', fetchResourcesImage);
   router.get('/resources/fetchRow', fetchAllImages);
+  router.get('/resources/fetchFile', fetchFileByTopic);
 };
